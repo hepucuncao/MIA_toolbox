1669ad5b0764a9884cb79372598fab618641039c
wjx189880 <chengaga2022@stu2022.jnu.edu.cn>
2024-12-26 09:05:23 +0800

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-setuptools.setup(
-    name='mia',
-    version='1.0',
-    description='this is a membership inference attack framework',
-    author='cjj',
-    author_email='2319128705@qq.com',
-    packages=setuptools.find_packages(),
-   )
